15a15a150b22b6360422f8888453941eba0bb697
Ghizlane.Ezz <phd.ezzarrad@gmail.com>
2024-04-09 13:37:48 -0400

    added code for task controllers

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -26,13 +26,58 @@ exports.postTask = async (req, res) => {
 };
 
 exports.getTask = async (req, res) => {
-  
+  try {
+    if (!validateObjectId(req.params.taskId)) {
+      return res.status(400).json({ status: false, msg: "Invalid task ID" });
+    }
+
+    const task = await Task.findOne({ user: req.user.id, _id: req.params.taskId });
+    if (!task) {
+      return res.status(404).json({ status: false, msg: "Task not found" });
+    }
+    res.status(200).json({ task, status: true, msg: "Task found successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ status: false, msg: "Internal Server Error" });
+  }
+
 };
 
 exports.putTask = async (req, res) => {
-  
+  try {
+    const { description } = req.body;
+    if (!description) {
+      return res.status(400).json({ status: false, msg: "Task description not provided" });
+    }
+
+    if (!validateObjectId(req.params.taskId)) {
+      return res.status(400).json({ status: false, msg: "Invalid task ID" });
+    }
+
+    const task = await Task.findByIdAndUpdate(req.params.taskId, { description }, { new: true });
+    if (!task) {
+      return res.status(404).json({ status: false, msg: "Task not found" });
+    }
+    res.status(200).json({ task, status: true, msg: "Task updated successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ status: false, msg: "Internal Server Error" });
+  }
+
 };
 
 exports.deleteTask = async (req, res) => {
-  
+  try {
+    if (!validateObjectId(req.params.taskId)) {
+      return res.status(400).json({ status: false, msg: "Invalid task ID" });
+    }
+    const task = await Task.findByIdAndDelete(req.params.taskId);
+    if (!task) {
+      return res.status(404).json({ status: false, msg: "Task not found" });
+    }
+    res.status(200).json({ status: true, msg: "Task deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ status: false, msg: "Internal Server Error" });
+  }
 };
